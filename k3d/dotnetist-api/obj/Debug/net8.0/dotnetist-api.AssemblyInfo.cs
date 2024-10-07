1ea406d378ea5171ab92df4ffea551873c12185b
emperoar <juliusbacosa@gmail.com>
2024-10-07 14:05:43 +0800

    ...

--- k3d/dotnetist-api/obj/Debug/net8.0/dotnetist-api.AssemblyInfo.cs
+++ k3d/dotnetist-api/obj/Debug/net8.0/dotnetist-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetist-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a505b4bd6c793993aed3e077269c8d51e5d757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17e1f854c7be3595423a72b53035a046d064f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetist-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetist-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
